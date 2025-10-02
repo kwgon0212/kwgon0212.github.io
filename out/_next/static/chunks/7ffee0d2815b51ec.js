c2e2509a126994b9e806aee8db1b09df612df0d9
kwgon0212 <kwgon0102@gmail.com>
2025-10-02 16:59:58 +0900

    Remove out folder from repository

--- out/_next/static/chunks/7ffee0d2815b51ec.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/6ffaa53a2f415c37.js",
-  "static/chunks/turbopack-6f7c9307d75e4254.js"
-])
--- out/_next/static/chunks/ab0749141387d5d1.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/6ffaa53a2f415c37.js",
-  "static/chunks/turbopack-c8170aef4377c7d4.js"
-])
